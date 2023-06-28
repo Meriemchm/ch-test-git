79905381b4da13ea5e3256468b89d4b75d71d97f
Meriemchm <chamimeriem2302@gmail.com>
2023-06-28 12:34:33 +0100

    minor refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-100">
-    Hello world!
-  </h1>
-  )
-}
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-100 flex justify-center items-center">
+        Hello world!
+      </h1>
+      <div className="grid grid-cols-2 md:grid-cols-4">
+        <div className="bg-blue-200">
+          <p>hey</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
