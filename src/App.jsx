a171e1e6dabcb28986dad30b11b952b7136f5a4b
Meriemchm <chamimeriem2302@gmail.com>
2023-06-29 14:30:14 +0100

    ajout fichier gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,7 @@ const App = () => {
       <div className="grid grid-cols-2 md:grid-cols-4">
         <div className="bg-blue-200 flex flex-col ">
           <h2>title</h2>
-          <p>description</p>
-
+          <img src="localstorage.JPG" alt="oui" />
         </div>
       </div>
     </>
